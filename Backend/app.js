771075ec98f731a6aac9ad4c3a679909613ecfbf
Wilfried GOMES FORTES <wgomesfortes@extralecbtlec.fr>
2025-05-16 15:04:44 +0200

    :package: :sparkles: add a connection database mysql

---
+++ Backend/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import db from './config/db.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  db.query('SELECT NOW()', (err, result) => {
+    if (err) return res.status(500).json({ error: 'Database connection error' });
+    res.json({ message: 'Database connected', date: result[0].now });
+  });
+});
+
+app.listen(3000, () => { 
+  console.log('Server is running on port 3000');
+});
