42fd6f10f6e3920283937b9e20b952c64e38d1cf
Wilfried GOMES FORTES <wgomesfortes@extralecbtlec.fr>
2025-05-21 14:22:01 +0200

    :sparkles: add a route and controllers Get procedures

--- Backend/controllers/proceduresControllers.js
+++ Backend/controllers/proceduresControllers.js
@@ -23,3 +23,35 @@ export const createProcedures = async (req, res) => {
     res.status(500).json({ success: false, message: 'Erreur serveur' });
   }
 };
+
+export const getProcedures = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const [procedures] = await db.query(
+      'SELECT * FROM procedures WHERE id = ?', 
+      [id]
+    );
+
+    if (procedures.length === 0) {
+      return res.status(404).json({ success: false, message: 'Procédures introuvable.' });
+    }
+
+    const procedure = procedures[0];
+
+    const [steps] = await db.query(
+      `SELECT * FROM process
+      WHERE procedures_id = ? 
+      ORDER BY position ASC`,
+      [id]
+    );
+    res.status(200).json({
+      success: true,
+      procedure,
+      steps
+    });
+  } catch (error) {
+    console.error('Erreur lors de la récupération :', error.message);
+    res.status(500).json({ success: false, message: 'Erreur serveur' });
+  }
+} ;
